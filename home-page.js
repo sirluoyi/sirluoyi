1576c869d4fb67b71393f906d6fc2dcb66995f23
Navneet Priyadarshi <navneetpriyadarshi@Navneets-Mac-mini.local>
2026-03-27 08:08:08 +0530

    update

--- home-page.js
+++ home-page.js
@@ -1,3 +1,5 @@
 document.body.querySelector('.js-navbar-button').addEventListener('click', () => {
   document.body.querySelector('.js-navbar').classList.toggle('slide');
+  document.body.querySelector('.js-navbar-button').classList.toggle('slide');
+  document.body.querySelector('.overlay').classList.toggle('slide');
 });
