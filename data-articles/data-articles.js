029edab9827157beb882755f657538cf0afabb47
Navneet Priyadarshi <navneetpriyadarshi@Navneets-Mac-mini.local>
2026-03-28 10:41:47 +0530

    update

---
+++ data-articles/data-articles.js
@@ -0,0 +1,132 @@
+
+export const dataArray = [
+	{
+			id: "philosophy-1",
+			category: "Philosophy",
+			heading: "9 Stoic Practices That Will Help You Thrive In The Madness Of Modernity",
+			headingThumb: "9 Stoic Practices That Will Help You Thrive In The Madness Of Modernity", 
+			subHeading: "Wisdom",
+			image: "stoic-article.webp",
+			imageThumb: "featured-post-1.jpg",
+			author: "John Spencer &middot March 21, 2026",
+			articleThumb: "Much of what happens in life is not within our control. The Stoics recognised this undeniable truth, and focused instead on what they could do. Born a slave, it would seem that Epictetus had no reason to believe he could control anything. He was permanently crippled from a broken leg given to him by his master. Epictetus would live and die in poverty...",
+
+			article: `
+			<p>When Zeno of Cyprus was shipwrecked and stranded on Athens, he wasn’t expecting any good to happen.</p>
+			<p>Having lost everything and with not much else to do, Zeno wandered into a bookshop and was quickly absorbed by the teachings of Socrates. After studying with the great philosophers of his time, he decided to impart his wisdom to anyone who would care to listen.</p>
+			<p>Thus the philosophy of Stoicism was born. Zeno’s teachings would quickly spread and would be adopted by both slaves and kings alike. As he would later joke: “I made a prosperous voyage when I suffered shipwreck.”</p>
+			<p>But that’s not where the story of Stoicism ends. Centuries later, the philosophy remains as relevant — if not more so — in modern society. These stoic practices will help bring calm to the chaos we face today.</p>
+
+			<p style="font-weight: bold">1. Develop An Internal Locus Of Control</p>
+
+			<p>Much of what happens in life is not within our control. The Stoics recognised this undeniable truth, and focused instead on what they could do.</p>
+			<p>Born a slave, it would seem that Epictetus had no reason to believe he could control anything. He was permanently crippled from a broken leg given to him by his master. Epictetus would live and die in poverty.</p>
+			<p>But that wasn’t what Epictetus thought. He would say that even while his property and even his body was not within his control, his opinions, desires, and aversions still remained his. That was something that he owned.</p>
+			<p>It’s easy to get frustrated today. We’re so used to comfort that even the slightest inconvenience provokes outrage within us. If the internet takes a second longer than it should or if traffic stalls for a minute, the natural instinct is annoyance if not rage.</p>
+			<p>It isn’t any of these breakdowns that are making us unhappy. The unhappiness stems from the emotional response that we have chosen. The onus is on ourselves to ensure that we don’t let external events affect our internal state of mind.</p>
+			<p>Once we internalise that, it becomes clear that we have the power to be happy regardless of our circumstances.</p>
+
+			<p style="font-weight: bold">2. Guard Your Time</p>
+
+			<p>The Stoics understood that time is our greatest asset. Unlike any of our material possessions, once lost, time can never be regained. We must therefore strive to waste as little of it as possible.</p>
+			<p>Those who squander this scarce resource on minutia or entertainment will find that they have nothing to show for it in the end. The habit of procrastination and putting things off will come back to haunt us. Tomorrow isn’t guaranteed.</p>
+			<p>On the other hand, those who give away their time freely to others will also find that they are no better than those who waste it. Most of us allow people and other obligations to impose on our time too easily. We make commitments without giving deep thought to what it entails. Calendars and schedules were meant to help us. We should not become a slave to them.</p>
+			<p>Regardless of which end of the spectrum we fall into, time is of the essence. We think we have a lot of time, but we really don’t.</p>
+
+			<p style="font-weight: bold">3. Don’t Outsource Your Happiness</p>
+
+			<p>Much of what we do stems from our primal need to be liked and accepted by others. Disapproval from our social group had serious repercussions in the past. It would have likely meant exile and eventually death in the wilderness.</p>
+			<p>That’s still true to some extent today. But how much time and effort do we spend trying to win the approval of others? What is it costing us?</p>
+			<p>We spend money we don’t have, to buy fancy things we don’t need, in order to impress someone we don’t care about. Our choice of career or lifestyle is centred around how others perceive us, rather than what is best for us. We are held hostage and pay a king’s ransom every day, with no guarantee that we will ever be free.</p>
+			<p>In contrast, the Roman statesman Cato sought to lead a life that was independent of the opinion of others. He would wear the most outlandish of outfits and walk in the streets without putting on shoes. It was his way of accustoming himself to be ashamed only of what deserves shame, and to despise all other sorts of disgrace.</p>
+			<p>That was the only way in which he could stand up to Julius Caesar, whom he recognised was consolidating too much power. It enabled him to make the big decisions when it counted, without fear of disapproval.</p>
+			<p>We have much to learn from him. Far better for us to live life on our own terms and ignore the opinions of others. Happiness should never be outsourced</p>
+
+			<p style="font-weight: bold">4. Stay Focused When Confronted With Distractions</p>
+
+			<p>Modern-day capitalism has given us an abundance of options.</p>
+			<p>Whether it’s food, travel, or entertainment, we have far more to work with than our predecessors did. Yet, this hasn’t clearly benefited us. When presented with so many options, we become paralysed by indecision.</p>
+			<p>This is known as the paradox of choice. Our brains haven’t been able to keep up with modern day advances and are overwhelmed when presented with so much information. Because it’s so difficult to make a choice, the default choice is to maintain the status quo.</p>
+			<p>It’s one of the core problems we face in our daily lives. With so many options, we never really commit to a path. We either put off making a decision or pursue multiple activities all at once. The result is that we never really make headway into anything at all.</p>
+			<p>The Stoics emphasised the need for purposeful action. We must take care not to be merely reacting to our circumstances, but to live intentionally.</p>
+
+			<p style="font-weight: bold">5. Toss Away Ego And Vanity</p>
+
+			<p>One of Epictetus’ biggest frustrations as a teacher was how his students claimed to be want to be taught, but secretly believed that they knew everything.</p>
+			<p>It’s a pain all teachers know and most of us would recognise. At the heart of it is ego and arrogance. The thought is that we’ve learnt enough and are better than our contemporaries.</p>
+			<p>Nowhere is such thinking more dangerous than today.</p>
+			<p>The information of today is not only insufficient for solving the problems of tomorrow but can very well be the obstacle for sharper thinking as well.We are in an age where we’re merely one step away from being disrupted in virtually every industry. Even in ancient times Marcus Aurelius has remarked, “the universe is change, life is an opinion”.</p>
+			<p>This is why the most brilliant minds of today spend a good portion of their time reading. They understand that there is always wisdom to be gleaned, whether from the past, present, or future.</p>
+			<p>We would be wise to do the same. Always stay a student.</p>
+
+			<p style="font-weight: bold">6. Consolidate Your Thoughts In Writing</p>
+
+			<p>Of the many things we can do daily, none are as important as looking inward. The act of self-reflection forces us to question ourselves and examine our own assumptions of the world. It’s how the answers to some of the world’s biggest questions have surfaced.</p>
+			<p>Keeping a journal remains one of the most effective ways for mindfulness. It boosts creativity, increases gratitude, and serves as therapy all at once. The benefits are numerous. Your thoughts and feelings become clearer in writing than in your mind.</p>
+			<p>The Stoics were well aware of that. The most powerful man in the Roman empire, Marcus Aurelius would dutifully take the time to record his observations and feelings whether at war or in peace. It’s what we know today as Meditations.</p>
+			<p>While everyone from athletes to entrepreneurs benefit from Marcus Aurelius’ wisdom today, it is clear that the biggest beneficiary of his writing and thinking was himself. The clarity of thought and accountability brought by his journal kept him virtuous when anyone in his position would have likely erred and become a tyrant.</p>
+			<p>Take the time to journal. It’s not difficult and the rewards are immense.</p>
+
+			<p style="font-weight: bold">7. Stand Your Ground</p>
+
+			<p>In a profession that is often based on compromise, Cato was stubborn and steadfast in his beliefs. He was taught that there were no shades of grey. All virtues were one and the same virtue, all vices the same vice.</p>
+			<p>It seems like an unreasonably high standard. It’s undeniable that many feats have only been made possible through compromise. Yet it seems that the pendulum has swung too far today: we forgo our principles in the name of tolerance or for profit.</p>
+			<p>Cato infuriated both his political allies and enemies for his sheer refusal to compromise. He demanded that his friends and family adopt the same stance, without leaving room for any flexibility. But adherence to this impossible standard also earned him unshakable authority. By default, he became Rome’s moral arbiter of right and wrong.</p>
+			<p>We can’t all be like him, but there is a lesson to be learnt. If you stand for nothing, you’ll fall for everything.</p>
+
+			<p style="font-weight: bold">8. Imagine The Worst That Could Happen</p>
+
+			<p>Much has been said about the power of positive thinking in recent times. We are taught that optimism and affirmations are the key to leading a happier life. But that’s not what the Stoics believed.</p>
+			<p>They felt that this practice invited passivity into our daily lives. It encourages us to simply hope for things to get better instead of taking concrete action. Rather than deny the harsh realities of life, they decided to embrace it.</p>
+			<p>They regularly conducted an exercise known as premeditatio malorum, which translates to a premeditation of evils. The goal was to imagine the worst events that could possibly happen to them. For some, it was a loss of reputation. To others, it was financial ruin and poverty. But common to all was the eventuality of death.</p>
+			<p>What would things look like if everything went wrong tomorrow?</p>
+			<p>How would I cope with that situation?</p>
+			<p>Should this change the way I live today?</p>
+			<p>These were some of the questions they asked themselves. The exercise never failed to yield valuable rewards. The Stoics took cautionary measures to ensure that the undesirable outcome would not eventuate. Even when it failed, they lived better for they had contemplated how they would weather the adversity they were faced with.</p>
+			<p>We should be brutally honest with ourselves and never be afraid to confront reality. That is the best way we can prepare for success and be ready for failure.</p>
+
+			<p style="font-weight: bold">9. Remember That Nothing Endures</p>
+
+			<p>In the grand scheme of things, none of what we’ve achieved matters.</p>
+			<p>It’s a sobering thought. We all experience the world like we are at the centre of reality. That creates an illusion where our importance is inflated. We see ourselves as the protagonist in our own story.</p>
+			<p>But the truth is this perception exists only in our minds. Everyone around us walks around with a similar mindset, but each of us are insignificant in the long run. Even the brightest minds such as Edison and Newton would eventually be relegated to a footnote.</p>
+			<p>There is no need for us to conform to irrational expectations and external pressures. Neither do we need to chase accomplishments in the hope of building a legacy. None of us these things last.</p>
+			<p>All that matters is we live life on our own terms. It is the only way we can truly say that we have lived a good life.</p>
+			`
+			
+	},
+
+	{
+			id: "mathematics-1",
+			category: "Mathematics",
+			heading: "Why the paradoxes of infinity still puzzle us today",
+			headingThumb: "Why the paradoxes of infinity still puzzle us today",
+			subHeading: "by Alan Lightman",
+			image: "infinity-article2.webp",
+			imageThumb: "featured-post-2.jpg",
+			author: "Alan Lightman &middot Sepctember 18, 2021",
+			articleThumb: "In Jorge Luis Borges story The Book of Sand, a mysterious stranger knocks on the door of the narrator and offers to sell him a Bible he came by in a small village in India. The book shows the wear of many hands. The stranger says that the illiterate peasant who gave him the book called it the Book of Sand, “because neither sand nor this book has a beginning or an end.”...",
+
+		 	article: `
+			<p>In Jorge Luis Borges’s story The Book of Sand, a mysterious stranger knocks on the door of the narrator and offers to sell him a Bible he came by in a small village in India. The book shows the wear of many hands. The stranger says that the illiterate peasant who gave him the book called it the Book of Sand, “because neither sand nor this book has a beginning or an end.” Opening the volume, the narrator finds that its pages are rumpled and badly set, with an unpredictable Arabic numeral in the upper corner of each page. The stranger suggests that the narrator try to find the first page. It is impossible.</p>
+			<p>No matter how close to the beginning he explores, several pages always remain between the cover and his hand. “It was as though they grew from the very book.” The stranger then asks the narrator to find the end of the book. Again, he fails. “It can’t be,” says the narrator. “It can’t be, but it is,” says the Bible peddler. “The number of pages in this book is literally infinite. No page is the first page; no page is the last.” The stranger pauses and reflects. “If space is infinite, we are anywhere, at any point in space. If time is infinite, we are at any point in time.” (Note to the observant reader: We cannot be at any point in time. Life can exist only during a relatively short period of cosmic history, as discussed in the last chapter.)</p>
+			<img src="../image/infinity-article.png" style="display: block; margin: 0 auto;">
+			<p>Thoughts of the infinite have mesmerized and confounded human beings through the millennia. For mathematicians, infinity is an intellectual playground, where an endless string of fractions can add up to 1. For astronomers, the question is whether outer space goes on and on and on and on ad infinitum. And if it does, as cosmologists now believe, unsettling consequences abound. For one, there should be an infinite number of copies of each of us somewhere out there in the cosmos. Because even a situation of minuscule probability—like the creation of a particular individual’s exact arrangement of atoms—when multiplied by an infinite number of trials, repeats itself an infinite number of times. Infinity multiplied by any number (except 0) equals infinity.</p>
+			<p>Measurements of infinity are impossible, or at least impossible according to the usual notions of size. If you cut infinity in half, each half is still infinite. If a weary traveler arrives at a fully occupied hotel of infinite size, no problem. You simply move the guest in room 1 into room 2, the guest in room 2 into room 3, and so on ad infinitum. In the process, you’ve accommodated all of the previous guests and freed up</p>
+			<p>room 1 for the new arrival. There’s always room at the infinity hotel. We can play games with infinity, but we cannot visualize infinity. By contrast, we can visualize flying horses. We’ve seen horses, and we’ve seen birds, so we can mentally implant wings on a horse and send it aloft. Not so with infinity. The unvisualizability of infinity is part of its mystique.</p>
+			<p>The first recorded conception of infinity seems to have occurred around 600 BC and is attributed to the Greek philosopher Anaximander, who used the word apeiron, meaning “unbounded” or “limitless.” For Anaximander, the Earth and the heavens and all material things were caused by the infinite, although infinity itself was not a material substance. Other ancient Greek philosophers held that infinity was a negative, even an evil, because the inability to measure a thing was considered a shortcoming of the thing—with the exception of the infinite and immeasurable One. About the same time as</p>
+			<p>Anaximander, the Chinese employed the word wuji, meaning “boundless,” and wuqiong, meaning “endless,” and believed that the infinite was very close to nothingness. (An interesting perspective on Pascal’s ideas, discussed in “Between Nothingness and Infinity.”) In Chinese thought, being and nonbeing, like yin and yang, are in harmony with each other—thus the kinship of infinity and nothingness.</p>
+			<p>A few centuries later, Aristotle argued that infinity does not actually exist. He conceded something he called potential infinity, such as the whole numbers. For any number, you can always create a bigger number by adding one to it. This process can continue as long as your stamina holds out, but you can never get to infinity. Indeed, one of the many intriguing properties of infinity is that you can’t get there from here. Infinity is not simply more and more of the finite. It seems to be of a completely different nature, although pieces of it may appear finite, like large numbers, or like large volumes of space. Infinity is a thing unto itself. All we see and experience has limits, boundaries, tangibilities. Not so with infinity. For similar reasons, St. Augustine, Spinoza, and other theological thinkers have associated infinity with God: the unlimited power of God, the unlimited knowledge of God, the unboundedness of God. “God is everywhere, and in all things, inasmuch as He is boundless and infinite,” said St. Thomas Aquinas.</p>
+			<p>Beyond the religious sphere of the immaterial world, physicists believe that there may be infinite things in the material world as well. But this belief can never be proven. You can’t get there from here. Most of us have our first glimmerings of infinity as children, when we look up at the night sky for the first time. Or when we go to sea, out of sight of land, and gaze upon the ocean extending on and on until it meets the horizon. But these are only glimmerings, like counting to a few thousand in Aristotle’s potential infinity. We’re overwhelmed. But we haven’t come close.</p>
+			<p>The concept of infinity remains a controversial and paradoxical topic today, galvanizing international conferences and heated scholarly disputes. Can physical forces ever be infinite in strength? Can physical space extend beyond galaxy after galaxy without limit? Is there an infinity between the infinity of the whole numbers and the infinity of all numbers? In May 2013, a panel of scientists and mathematicians gathered in New York City to discuss the profound conundrums surrounding infinity. William Hugh Woodin, a mathematician at the University of California, Berkeley, put it this way: “It’s kind of like mathematics lives on a stable island—we’ve built a solid foundation. Then, there’s the wild land out there. That’s infinity.”</p>
+			`
+	}
+
+
+
+
+
+
+
+
+];
---
+++ scripts/explore-articles-list.js
@@ -0,0 +1,28 @@
+import { dataArray } from "../data-articles/data-articles.js";
+
+const body = document.body.querySelector('.js-data-import');
+
+let html = ``;
+
+	dataArray.forEach(item => {
+		
+		let itemHtml = `
+			<a href="philosophy.html" class="article1">
+      <div class="article1-box1">
+        <img src="../image/${item.imageThumb}">
+      </div>
+      <div class="article1-box2">
+        <div class="article-category">${item.category}</div>
+        <div class="article-title">${item.headingThumb}</div>
+        <div class="article-main">${item.articleThumb}</div>
+        <div class="article-footer">${item.author}</div>
+      </div>
+    </a>
+		`;
+
+		html += itemHtml;
+
+	});
+
+	body.innerHTML = html;
+
